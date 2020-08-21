ea5a37e585be619d7821f2524da7b5603f550fc2
Oskar <oskarrodholm@msn.com>
2020-08-21 09:16:35 +0200

    remove dactyl_manuform specific code

--- keyboards/maartenwut/nt660/keymaps/colemak_qwerty/config.h
+++ keyboards/maartenwut/nt660/keymaps/colemak_qwerty/config.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#define USE_SERIAL
-
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-//#define EE_HANDS
-// Rows are doubled-up
-
 // Default tapping term is 200
 #define TAPPING_TERM 180
 #define TAPPING_TERM_PER_KEY
@@ -22,20 +15,3 @@
 
 #define COMBO_COUNT 11
 #define COMBO_TERM 70
-
-
-//Shamelessly stolen mouse key settings from @pierrechevalier83:
-
-// Set the mouse settings to a comfortable speed/accuracy trade-off
-// Assume the screen refresh rate is 60 Htz or higher
-// The default is 50. This makes the mouse ~3 times faster and more accurate
-// #undef to override default settings
-#undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL 16
-// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-// give it more time to accelerate to max speed to retain precise control over short distances.
-#undef MOUSEKEY_TIME_TO_MAX 
-#define MOUSEKEY_TIME_TO_MAX 40
-// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY 100
