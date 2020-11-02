78b969ee069a8dc707647632a1e6191e8ca32d5e
Ditt Namn <du@example.com>
2020-11-02 21:54:04 +0100

    Insert oskar keymap into crkbd [prev gotham keymap]

--- keyboards/crkbd/keymaps/oskar/keycodes.h
+++ keyboards/crkbd/keymaps/oskar/keycodes.h
@@ -1,14 +1,17 @@
 #pragma once
 
 enum layers {
-    _QWERTY,
+    _COLEMAK_DHM_SWE,
     _LOWER,
     _RAISE,
     _ADJUST,
 };
 
-enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST };
+enum custom_keycodes { COLEMAK_DHM_SWE = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST, //Gotham keycodes
+	               ALT_TAB, NO_D_TILDE, NO_D_CIRC, NO_D_GRV //Oskar keycodes
+};
 
+// Gotham defines
 #define SFT_EQ MT(MOD_LSFT, KC_EQL)
 #define SFT_QT MT(MOD_RSFT, KC_QUOT)
 
@@ -21,8 +24,21 @@ enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST };
 #define LOW_SPC LT(_LOWER, KC_SPC)
 #define RAI_EQ LT(_RAISE, KC_EQL)
 #define RAI_ENT LT(_RAISE, KC_ENT)
+#define LOW_ENT LT(_LOWER, KC_ENT)
 #define LOW_BSP LT(_LOWER, KC_BSPC)
 #define LOW_DEL LT(_LOWER, KC_DEL)
 #define ADJ_GRV LT(_ADJUST, KC_GRV)
 #define KC_ANGL LSFT(KC_COMM)
-#define KC_ANGR LSFT(KC_DOT)
+#define KC_ANGR LSFT(KC_DOT
+
+// Oskar defines
+// Left-hand home row mods
+#define HOME_S LSFT_T(SE_S)
+#define HOME_T LCTL_T(SE_T)
+
+// Right-hand home row mods
+#define HOME_E RSFT_T(SE_E)
+#define HOME_N LCTL_T(SE_N)
+
+#define C_INS LCTL(KC_INS)
+#define S_INS LSFT(KC_INS)
