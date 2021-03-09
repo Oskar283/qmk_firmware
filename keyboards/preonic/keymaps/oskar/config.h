f9837f077674f8258ab0d1461be3879209ef3141
Oskar <oskarrodholm@msn.com>
2021-03-09 20:33:07 +0100

    Preonic: Add homerow mods config

--- keyboards/preonic/keymaps/oskar/config.h
+++ keyboards/preonic/keymaps/oskar/config.h
@@ -1,5 +1,21 @@
 #pragma once
 
+
+
+// Home row mods settings
+// Default tapping term is 200
+#define TAPPING_TERM 150
+#define TAPPING_TERM_PER_KEY
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD
+
+// This prevents short hold periods to be interpreted as individual taps when typing quickly.
+#define PERMISSIVE_HOLD
+
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
